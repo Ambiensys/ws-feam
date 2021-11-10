14d4e3b27a6591690c835e08ec28b058d31de906
jeffersonfersa <49044966+jeffersonfersa@users.noreply.github.com>
2021-11-10 15:38:07 -0300

    Implemetação do buscarArmazenador

--- src/main/java/ws/integracao/feam/resource/FeamResource.java
+++ src/main/java/ws/integracao/feam/resource/FeamResource.java
@@ -14,6 +14,7 @@ import org.jboss.logging.Logger;
 
 import io.smallrye.common.constraint.NotNull;
 import ws.integracao.feam.modelo.endereco.FeamEndereco;
+import ws.integracao.feam.modelo.parceiro.FeamParceiroResposta;
 import ws.integracao.feam.service.FeamService;
 
 @Path("/feam/api/v1")
@@ -38,6 +39,29 @@ public class FeamResource {
             FeamEndereco feamEndereco = null;
             feamEndereco = service.buscarEndereco("buscaCep", cep);
             return feamEndereco;
+        
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+	
+	@POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/buscarArmazenador")
+    public FeamParceiroResposta buscarArmazenador( 
+    				     @NotBlank(message = "CNPJ não pode ser vazio") 
+                         @NotNull()
+                         @QueryParam("cnpj") final String cnpj,
+                         @NotBlank(message = "Cookie não pode ser vazio") 
+					     @NotNull()
+					     @QueryParam("cookie") final String cookie) {
+        logger.info("buscarArmazenador");
+        try {
+        	FeamParceiroResposta feamParceiroResposta = null;
+        	feamParceiroResposta = service.buscarArmazenador("buscaPessoaPorTipo", cnpj, "2", "", "S", cookie);
+            return feamParceiroResposta;
         
         } catch (Exception e) {
             e.printStackTrace();
